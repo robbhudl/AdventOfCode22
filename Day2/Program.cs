41dd68e1337c1ded37deec4b15f98aeb73578f0a
Rob Blankson <robert.blankson@hudl.com>
2022-12-04 17:53:01 +0000

    Day 2

---
+++ Day2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Linq;
+
+namespace DayTwo
+{
+    class Program
+    {
+        static Dictionary<char, int> scores = new Dictionary<char, int> 
+        {
+            {'X', 1}, // Rock
+            {'Y', 2}, // Paper
+            {'Z', 3}, // Scissors
+        };
+
+        static int win = 6;
+        static int draw = 3;
+        static int lose = 0;
+
+        static int CalculateResult(char oppo, char me)
+        {
+            var baseScore = scores[me];
+            switch (oppo)
+            {
+                case 'A': // Rock
+                    if (me == 'X') { return draw + baseScore; }
+                    if (me == 'Y') { return win + baseScore; }
+                    if (me == 'Z') { return lose + baseScore; }
+
+                break;
+                
+                case 'B': // Paper
+                    if (me == 'X') { return lose + baseScore; }
+                    if (me == 'Y') { return draw + baseScore; }
+                    if (me == 'Z') { return win + baseScore; }
+                    
+                break;
+
+                case 'C': // Scissors
+                    if (me == 'X') { return win + baseScore; }
+                    if (me == 'Y') { return lose + baseScore; }
+                    if (me == 'Z') { return draw + baseScore; }
+                    
+                break;
+
+                default:
+                    return 0;
+            }
+
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            var totalScore = 0;
+
+            foreach (var line in System.IO.File.ReadLines(@"strategy.txt"))
+            {
+                var chars = line.ToCharArray();
+                // Console.WriteLine(chars.Length);
+                var oppo = chars[0];
+                var me = chars[2];
+
+                totalScore += CalculateResult(oppo, me);
+            }
+
+            Console.WriteLine($"Total score = {totalScore}");
+        }
+    }
+}
