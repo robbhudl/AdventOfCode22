2b5a0e0f95db72108af73d678c1ed6c33a3cddb8
Rob Blankson <robert.blankson@hudl.com>
2022-12-04 16:50:09 +0000

    Add top 3

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,11 +1,15 @@
-﻿namespace HelloWorld
+﻿using System.Collections;
+using System.Linq;
+
+namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var maxCalories = 0;
-            var currentCalories = 0;
+            var maxCalories = 0m;
+            var currentCalories = 0m;
+            var calories = new List<decimal>();
 
             // Read the file and display it line by line.  
             foreach (var line in System.IO.File.ReadLines(@"calories.txt"))
@@ -13,15 +17,20 @@
                 if (string.IsNullOrWhiteSpace(line))
                 {
                     maxCalories = currentCalories > maxCalories ? currentCalories : maxCalories;
+                    calories.Add(currentCalories);
                     currentCalories = 0;
                     continue;
                 }
 
                 currentCalories += int.Parse(line);
-                // System.Console.WriteLine(currentCalories);  
-            }  
+            }
 
-            Console.WriteLine(maxCalories);
+            calories = calories.OrderByDescending(c => c).ToList();
+            calories.Take(3).Sum();
+            var topThreeCalories = calories.Take(3).Sum();;
+
+            Console.WriteLine($"Individual elf maxCalories = {maxCalories}");
+            Console.WriteLine($"Top 3 elf maxCalories = {topThreeCalories}");
         }
     }
 }
