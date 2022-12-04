1160993cc5750508d1846bb7cf510993247a86e4
Rob Blankson <robert.blankson@hudl.com>
2022-12-04 16:11:40 +0000

    Day 1 solution

---
+++ Day1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var maxCalories = 0;
+            var currentCalories = 0;
+
+            // Read the file and display it line by line.  
+            foreach (var line in System.IO.File.ReadLines(@"calories.txt"))
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    maxCalories = currentCalories > maxCalories ? currentCalories : maxCalories;
+                    currentCalories = 0;
+                    continue;
+                }
+
+                currentCalories += int.Parse(line);
+                // System.Console.WriteLine(currentCalories);  
+            }  
+
+            Console.WriteLine(maxCalories);
+        }
+    }
+}
