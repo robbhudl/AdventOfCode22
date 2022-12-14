f8e7314845b220f230663f6ac214d49faf942bfd
Rob Blankson <robert.blankson@hudl.com>
2022-12-14 12:59:48 +0000

    Day13 Nearly there
    Doesn't work if right was a single int
    Need to track that somehow

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -1,53 +1,106 @@
-﻿var packetPairs = new List<List<string>> { new List<string>() };
+﻿var pairs = new List<List<string>> { new List<string>() };
 
 foreach (var line in System.IO.File.ReadLines(@"signalstest.txt"))
 {
     if (!string.IsNullOrWhiteSpace(line))
     {
-        packetPairs.Last().Add(line);
+        var newLine = line.Substring(1);
+        pairs.Last().Add(newLine);
     }
     else
     {
-        packetPairs.Add(new List<string>());
+        pairs.Add(new List<string>());
     }
 }
 
-var signal = ParseSignal(packetPairs[7][0]);
+foreach (var pair in pairs)
+{
+    var packetLeft = ParseSignal(pair[0]);
+    var packetRight = ParseSignal(pair[1]);
+
+    var isCorrectOrder = ComparePackets(packetLeft, packetRight);
+}
 
 var orderedCorrectly = new List<bool>();
 
-Console.WriteLine($"Number of pairs = {packetPairs.Count}");
+Console.WriteLine($"Number of pairs = {pairs.Count}");
 
-List<List<Object>> ParseSignal(string signal)
+List<List<int>> ParseSignal(string signal)
 {
-    var referenceStack = new Stack<List<Object>>();
-    var currentList = new List<Object>();
-    var packetData = new List<List<Object>> { currentList };
+    var numbers = new List<List<int>>();
+    List<int>? currentList = null; // new List<int>();
+    var previousChar = ' ';
+
     foreach (var character in signal)
     {
         switch (character)
         {
             case '[':
-                var newList = new List<Object>();
-                referenceStack.Push(newList);
-                currentList?.Add(newList);
+                var newList = new List<int>();
                 currentList = newList;
+                numbers.Add(currentList);
                 break;
 
             case ',':
                 continue;
 
             case ']':
-                referenceStack.Pop();
-                referenceStack.TryPeek(out currentList);
+                currentList = null;
                 break;
             
             default:
+                if (currentList == null)
+                {
+                    numbers.Add(new List<int>());
+                    currentList = numbers.Last();
+                }
                 var val = (int)char.GetNumericValue(character);
-                currentList?.Add(val);
+                currentList.Add(val);
                 break;
         }
+        previousChar = character;
     }
 
-    return packetData;
+    return numbers;
+}
+
+bool ComparePackets(List<List<int>> packetLeft, List<List<int>> packetRight)
+{
+    for (int i = 0; i < packetLeft.Count; i++)
+    {
+        var left = packetLeft[i];
+        var right = packetRight[i];
+
+        // if (left.Count < right.Count)
+        // {
+        //     return false;
+        // }
+
+        int index = 0;
+        foreach (var valLeft in left)
+        {
+            if (index == right.Count)
+            {
+                return true;
+            }
+            var valRight = right[index];
+            if (!CompareValues(valLeft, valRight))
+            {
+                return false;
+            }
+            index++;
+        }
+    }
+    return true;
+}
+
+bool CompareValues(int left, int right)
+{
+    return left <= right;
+}
+
+class PacketData
+{
+    public List<int> Values { get; set; }
+    bool IsSingleInt { get; set; }
 }
